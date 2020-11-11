0102cdd8030463a6c84f71f02685e5ba20d5aa6c
Liam Walsh <liw6800@gmail.com>
2020-11-11 01:27:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
 def fizzbuzz (int)
   if int % 3 == 0
     puts "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  if int % 15 == 0
+  elsif int % 15 == 0
     puts "FizzBuzz"
+  else 
+    puts int  
 end  
